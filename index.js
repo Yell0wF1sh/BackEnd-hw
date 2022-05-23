90d34333fd1ea57e42b343360277ab8d1b560180
Yell0wF1sh <86252839+Yell0wF1sh@users.noreply.github.com>
2022-05-23 14:29:48 -0400

    1

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 3000;
+
+app.listen(PORT, () => console.info("Server has started on", PORT));
 
 let newShip = require("./routes/newShip");
 
@@ -11,4 +13,4 @@ let newShip = require("./routes/newShip");
 app.use(bodyParser.json());
 // on a request to /, use the newShip code.
 // note that the entire URL including the one declared in the file must match.
-app.use("/",newShip);
+app.use("/", newShip);
--- routes/newShip.js
+++ routes/newShip.js
@@ -5,33 +5,33 @@ let Ship = require("../models/Ship");
 /**
  * tell Express.js that when it receives a POST request at the URL /newShip/, to do this code.
  */
-router.post("/newShip/", function(req, res){
+router.post("/newShip/", function (req, res) {
   // look up documents in MongoDB by name.
-  Ship.findOne({name: req.body.name}, function(error,doc){
+  Ship.findOne({ name: req.body.name }, function (error, doc) {
     // if there was an error
-    if(error){
-      console.error("Error finding ship",error);
+    if (error) {
+      console.error("Error finding ship", error);
       res.status(500).send(error);
     }
     // if no document was found
-    else if(!doc){
+    else if (!doc) {
       // create a new instance of the Ship model, using the request body as the data.
       new Ship(req.body).save((err, doc) => {
         /**
          * this error/document fat-arrow function is required.
          * on an error, handle it. else send the newly created document back to the client.
          */
-        if(err){
-          console.error("Error saving new ship",err);
+        if (err) {
+          console.error("Error saving new ship", err);
           res.status(500).send(err);
         }
-        else{
+        else {
           res.send(doc);
         }
       });
     }
     // a document was found, return it instead.
-    else{
+    else {
       res.send(doc);
     }
   });
