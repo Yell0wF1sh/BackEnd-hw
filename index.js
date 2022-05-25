71e7e61a3655aed5df78742d2672304feb0b17f6
Yell0wF1sh <86252839+Yell0wF1sh@users.noreply.github.com>
2022-05-25 15:17:08 -0400

    6

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const PORT = 3000;
 app.listen(PORT, () => console.info("Server has started on", PORT));
 
 let newShip = require("./routes/newShip");
+let newCar = require("./routes/newCar");
 
 // on every request, parse the request body using this library.
 app.use(bodyParser.json());
 // on a request to /, use the newShip code.
 // note that the entire URL including the one declared in the file must match.
 app.use("/", newShip);
+app.use("/", newCar);
