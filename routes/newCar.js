ecdc2bcfb39285d741acd9ed4868569523ac5466
Yell0wF1sh <86252839+Yell0wF1sh@users.noreply.github.com>
2022-05-25 15:14:24 -0400

    5

---
+++ routes/newCar.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+let Car = require("../models/Car");
+
+router.post("/newCar", function (req, res) {
+  res.send("Successful connection!");
+});
--- routes/newShip.js
+++ routes/newShip.js
@@ -5,7 +5,7 @@ let Ship = require("../models/Ship");
 /**
  * tell Express.js that when it receives a POST request at the URL /newShip/, to do this code.
  */
-router.post("/newShip", function (req, res) {
+router.post("/newShip/", function (req, res) {
   // look up documents in MongoDB by name.
   Ship.findOne({ name: req.body.name }, function (error, doc) {
     // if there was an error
@@ -37,4 +37,65 @@ router.post("/newShip", function (req, res) {
   });
 });
 
+/**
+ * tell Express.js that when it receives a GET request at the URL /getShip/, to do this code.
+ */
+router.get("/getShip/", function (res, req) {
+  // look up documents in MongoDB by name (provided by the field name in the request).
+  Ship.findOne({ name: req.body.name }, function (e, doc) {
+    // if there's an error while finding the ship
+    if (e) {
+      console.error("Error finding ship", e);
+      res.status(500).send(e);
+    }
+    // if the method finds no existing document with the name
+    else if (!doc) {
+      console.error("There's no such ship in the database", e);
+      res.status(404).send(e);
+    }
+    // a document was found, return it.
+    else {
+      res.send(doc);
+    }
+  });
+});
+
+router.get("/getShip/secondaryBattery", function (res, req) {
+  Ship.find({ secondaryBattery: req.body.secondaryBattery }, function (e, doc) {
+    if (e) {
+      console.error("Error finding ship", e);
+      res.status(500).send(e);
+    }
+    // if the method finds no existing document with the name
+    else if (!doc) {
+      console.error("There's no such ship in the database", e);
+      res.status(404).send(e);
+    }
+    // a document was found, return it.
+    else {
+      res.send(doc);
+    }
+  });
+});
+
+router.patch("/updateShip", function (res, req) {
+  if (req.body.name == null) {
+    res.status(400);
+  }
+  else {
+    Ship.findOneAndUpdate({ name: req.body.name }, function (e, docs) {
+      if (e) {
+        console.error("Error finding ship", e);
+        res.status(500).send(e);
+      }
+      else if (!docs) {
+        console.error("There's no such ship in the database", e);
+        res.status(404).send(e);
+      }
+      else {
+        res.send(docs);
+      }
+    });
+  }
+});
 module.exports = router;
